ce923957a2044a39d6d1d6efa79fd07a4967a346
william <william.frazao91@gmail.com>
2025-01-06 18:10:09 -0300

    feat: create context

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import Header from "./Components/Header";
 import Sidenav from "./Components/Sidenav";
 import Resumo from "./Pages/Resumo";
+import { DataContextProvider } from "./Context/DataContext";
 import "./Style.css";
 
 function App() {
   return (
-  <div>
-    <Sidenav />
-    <main>
-      <Header />
-      <Resumo />
-    </main>
-  </div>
+    <DataContextProvider>
+    <div>
+      <Sidenav />
+      <main>
+        <Header />
+        <Resumo />
+      </main>
+    </div>
+  </DataContextProvider>
   );
 }
 
