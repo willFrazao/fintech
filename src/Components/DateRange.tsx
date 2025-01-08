467556153aae698274951e9a3ac107755c3752db
william <william.frazao91@gmail.com>
2025-01-08 15:45:30 -0300

    feat: CSS Base

--- src/Components/DateRange.tsx
+++ src/Components/DateRange.tsx
@@ -6,7 +6,7 @@ const DateRange = () => {
   const [final, setFinal] = React.useState("");
 
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
+    <form className='box flex' onSubmit={(e) => e.preventDefault()}>
       <DateInput 
         label="Início" 
         value={inicio}
