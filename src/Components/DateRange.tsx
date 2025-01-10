553e06b07f95e295cdbe9d68fcd98a26c0193163
william <william.frazao91@gmail.com>
2025-01-10 12:20:41 -0300

    feat: botao dos meses

--- src/Components/DateRange.tsx
+++ src/Components/DateRange.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import DateInput from './DateInput';
 import { useData } from '../Context/DataContext'
 
 const DateRange = () => {
   const { inicio, setInicio, final, setFinal} = useData();
-  
+
   return (
     <form className='box flex' onSubmit={(e) => e.preventDefault()}>
       <DateInput 
@@ -12,13 +11,11 @@ const DateRange = () => {
         value={inicio}
         onChange={({ target }) => setInicio(target.value)}
       />
-        {inicio}
       <DateInput 
         label="Final" 
         value={final}
         onChange={({ target }) => setFinal(target.value)}
       />
-      {final}
     </form>
   )
 }
