a96d1da4af0b364fba051038050fb8659bd03877
william <william.frazao91@gmail.com>
2025-01-16 08:43:31 -0300

    feat: pagina de resumo de vendas

--- src/Pages/Resumo.tsx
+++ src/Pages/Resumo.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { useData } from '../Context/DataContext';
 
 const Resumo = () => {
+  const { data } = useData();
+  if (data === null) return null;
   return (
-    <div>Resumo</div>
+    <section>
+      <div className='resumo flex mb'>
+        <div className='box'>
+          <h2>Vendas</h2> 
+          <span>
+            {data
+              .filter((item) => item.status !== "falha")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL"})
+            }
+          </span>
+        </div>
+
+        <div className='box'>
+          <h2>Recebido</h2>
+          <span>
+            {data
+              .filter((item) => item.status === "pago")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL"})
+            }
+          </span>
+        </div>
+
+        <div className='box'>
+          <h2>Processando</h2>
+          <span>
+            {data
+              .filter((item) => item.status === "processando")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL"})
+            }
+          </span>
+        </div>
+      </div>
+      <div className='box'>Gráficos</div>
+    </section>
   )
 }
 
